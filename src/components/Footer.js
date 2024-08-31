4657cb8d106e96cbdff6e9856c05fe8e0df90ed0
DAlejandroB <diegoaenciso76@gmail.com>
2024-08-31 12:55:34 -0500

    refactor: Update Footer component with dynamic social profiles and responsive design improvements
    - Replaced hardcoded social media links with a dynamic array of profile objects for better maintainability.
    - Added icons for each social profile using react-icons.
    - Implemented responsive text hiding for social profile names on smaller screens using Bootstrap utility classes.
    - Added padding to the last column on smaller screens to improve vertical spacing.
    - Improved overall code organization and readability.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Container, Row, Col, Stack, Nav } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { FaGithub, FaLinkedin, FaStackOverflow  } from 'react-icons/fa';
+
+const socialProfiles = [
+  { platform: "GitHub", url: "https://github.com/DAlejandroB", icon: <FaGithub size={30}/> },
+  { platform: "LinkedIn", url: "https://www.linkedin.com/in/dialbaen/", icon: <FaLinkedin size={30}/> },
+  { platform: "Stack Overflow", url: "https://stackoverflow.com/users/15344638/diegoalejandrob", icon: <FaStackOverflow size={30}/> },
+];
 
 function Footer() {
   return (
     <footer>
       <Container fluid>
-        <Row className="bg-secondary text-white p-4">
+        <Row className="bg-secondary text-light p-4">
           <Col className="mx-md-5">
             <Stack className="text-start">
               <h3>Diego Ballesteros</h3>
@@ -17,15 +23,18 @@ function Footer() {
           <Col>
             <Nav className="flex-column fs-5 text-start">
               <span>Check out my:</span>
-              <Nav.Link href="https://www.linkedin.com/in/dialbaen/" className="text-white" aria-label="LinkedIn Profile">LinkedIn</Nav.Link>
-              <Nav.Link href="https://stackoverflow.com/users/15344638/diegoalejandrob" className="text-white" aria-label="Stack Overflow Profile">Stack Overflow</Nav.Link>
-              <Nav.Link href="https://github.com/DAlejandroB" className="text-white" aria-label="GitHub Profile">GitHub</Nav.Link>
+              {socialProfiles.map((profile) => (
+                <Nav.Link href={profile.url} className="text-light" aria-label={`Link to my ${profile.platform}`}>
+                  {profile.icon}
+                  <span className='d-none d-md-inline p-2'>{profile.platform}</span>
+                </Nav.Link>
+              ))}
             </Nav>
           </Col>
           <Col>
-            <Stack className="text-start">
-              <p className="mb-4">Page created using React and JavaScript, for more info, please contact <a href="mailto:dalejandrob99@gmail.com" className="text-white">dalejandrob99@gmail.com</a></p>
-              <p>2024</p>
+            <Stack className="text-start pt-4">
+              <p className="mb-4">Page created using React and JavaScript, for more info, please contact <a href="mailto:dalejandrob99@gmail.com" className="text-light">dalejandrob99@gmail.com</a></p>
+              <p>2024. All rights reserved by Diego Ballesteros</p>
             </Stack>
           </Col>
         </Row>

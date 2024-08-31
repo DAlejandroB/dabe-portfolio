29f5246a0ed62cfeb97a65ec45d1810cde9a0c0c
DAlejandroB <diegoaenciso76@gmail.com>
2024-08-30 23:23:20 -0500

    refactor: Integrate newly created components and pages into App.js and index.js
    - Imported Footer, Header, and newly created pages (About, Contact, Experience, Home, Skills) into App.js
    - Updated App.js to include routing for the new pages
    - Adjusted index.js to correctly render the App component with the new structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Experience from './pages/Experience';
+import Skills from './pages/Skills';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App d-flex flex-column min-vh-100" id="root">
+      <Header />
+      <div className="flex-grow-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/skills" element={<Skills />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 }
 
 export default App;
+

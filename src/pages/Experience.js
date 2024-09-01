ab982b2c1b941358d0c41171aa7fbb50871d6fee
DAlejandroB <diegoaenciso76@gmail.com>
2024-09-01 17:25:54 -0500

    feat: Populate Experience page with dynamic content
    - Replaced placeholder text in the `Experience` component with dynamic content.
    - Created an `experiences` array to hold job experience data including title, date, description, and listed skills.
    - Integrated the `ExperienceItem` component to render each experience dynamically within the `Experience` component.
    - Applied padding to the experience list container for better spacing.
    
    These updates dynamically display job experiences on the Experience page, enhancing the content management and user experience.

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import ExperienceItem from '../components/ExperienceItem';
+
+const experiences = [
+  {
+    title: "Fenix Alliance",
+    date: "Aug 2023 - Sep 2024",
+    description: "Refactored the backend codebase to implement REST APIs as the data access layer using ASP.NET and Entity Framework Core. Designed and developed multiple frontend components in React.js and Blazor. Achieved near-full test coverage for both the backend and frontend components, ensuring high code quality and reliability.",
+    listedSkills: ["C# ASP.NET Core", "Entity Framework", "Blazor", "React", "JavaScript"]
+  },
+  {
+    title: "Banco de Bogotá",
+    date: "Feb 2023 - Aug 2023",
+    description: "Streamlined Security Operations Center (SOC) internal processes by delivering automation solutions. Leveraged several development tools across the entire development lifecycle to enhance efficiency and operational effectiveness.",
+    listedSkills: ["Python" , "Node.js",  "HTML/CSS", "VBA Excel", "PowerBi & PowerAutomate", "MS Active Directory"]
+  },
+  {
+    title: "Freelance Developer",
+    date: "Jul 2022 - Present",
+    description: "Worked on several full-stack software solutions in both solo and team-based projects. Developed and deployed applications, ensuring robust backend and frontend functionality. Ensured API quality through rigorous testing and documentation. Worked on backend services using various frameworks and managed data with relational and non-relational databases.",
+    listedSkills: ["Java Spring Boot", "React", "Node.js", "Express", "C# ASP.NET Core", "Postman", "Swagger", "SQL/NoSQL Databases", "Azure", "Git", "Agile - Scrum"]
+  }  
+];
 
 const Experience = () => {
   return (
     <div>
-      <h1>This is the Experiences page!</h1>
+      <h1>These are my job Experiences!</h1>
+      <div className="py-6">
+        {experiences.map((exp, index) => (
+          <ExperienceItem
+            key={index}
+            title={exp.title}
+            date={exp.date}
+            description={exp.description}
+            listedSkills={exp.listedSkills}
+          />
+        ))}
+      </div>
     </div>
   );
 };
